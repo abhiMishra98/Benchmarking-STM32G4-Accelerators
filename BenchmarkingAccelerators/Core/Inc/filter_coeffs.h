c6d405bc5b40c912616a407e663902512d5fdd83
Abhishek Mishra <abhimishra.cetb@gmail.com>
2025-08-18 19:37:40 +0200

    Add FIR FMAC, with validation

--- BenchmarkingAccelerators/Core/Inc/filter_coeffs.h
+++ BenchmarkingAccelerators/Core/Inc/filter_coeffs.h
@@ -3,9 +3,7 @@
 
 #include <stdint.h>
 
-#define EMA_NUM_B_COEFFS 1
-#define EMA_NUM_A_COEFFS 1
-extern int16_t ema_b_coeffs[EMA_NUM_B_COEFFS];
-extern int16_t ema_a_coeffs[EMA_NUM_A_COEFFS];
+#define NUM_TAPS 61
+extern int16_t fir_coeffs[NUM_TAPS];
 
-#endif // IIR_COEFFS_H
+#endif // FILTER_COEFFS_H
